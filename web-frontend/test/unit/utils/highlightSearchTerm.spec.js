5391e071892d2ba13015fd115c6a767f1a0fb73e
Pedro Ramos Sousa Reis <pedro.rsr0308@gmail.com>
2026-05-28 19:06:04 -0300

    test(search): RED - teste de multiplas ocorrencias

--- web-frontend/test/unit/utils/highlightSearchTerm.spec.js
+++ web-frontend/test/unit/utils/highlightSearchTerm.spec.js
@@ -6,4 +6,10 @@ describe('highlightSearchTerm', () => {
     const result = highlightSearchTerm('Olá Baserow', 'Baserow')
     expect(result).toBe('Olá <mark>Baserow</mark>')
   })
+
+  // Substitui múltiplas ocorrências
+  test('substitui TODAS as ocorrências', () => {
+    const result = highlightSearchTerm('oi oi oi', 'oi')
+    expect(result).toBe('<mark>oi</mark> <mark>oi</mark> <mark>oi</mark>') // [cite: 421, 422, 423, 424]
+  })
 })
