f505c6763600c8c66bb7102b6565885db9c5a8c3
Pedro Ramos Sousa Reis <pedro.rsr0308@gmail.com>
2026-06-01 10:57:35 -0300

    feat(search): GREEN - substituicao dinamica de tags html

--- web-frontend/modules/core/utils/search.js
+++ web-frontend/modules/core/utils/search.js
@@ -1,5 +1,6 @@
 export function highlightSearchTerm(text, term, options = {}) {
+  const tag = options.tag || 'mark'
   const flags = options.caseSensitive ? 'g' : 'gi'
   const regex = new RegExp(term, flags)
-  return text.replace(regex, (match) => `<mark>${match}</mark>`)
+  return text.replace(regex, (match) => `<${tag}>${match}</${tag}>`)
 }
