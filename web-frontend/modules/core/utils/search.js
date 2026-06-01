0163001805c33a83c5cf0bcc5dd617fbd4248158
Pedro Ramos Sousa Reis <pedro.rsr0308@gmail.com>
2026-06-01 10:50:06 -0300

    feat(search): GREEN - logica de alternancia de flags case-sensitive

--- web-frontend/modules/core/utils/search.js
+++ web-frontend/modules/core/utils/search.js
@@ -1,4 +1,5 @@
-export function highlightSearchTerm(text, term) {
-  const regex = new RegExp(term, 'gi')
+export function highlightSearchTerm(text, term, options = {}) {
+  const flags = options.caseSensitive ? 'g' : 'gi'
+  const regex = new RegExp(term, flags)
   return text.replace(regex, (match) => `<mark>${match}</mark>`)
 }
