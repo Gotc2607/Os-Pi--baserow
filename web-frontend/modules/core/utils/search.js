c355d43c12e43e9fff53f88f7bb6077c8813c453
Pedro Ramos Sousa Reis <pedro.rsr0308@gmail.com>
2026-06-01 11:20:21 -0300

    feat(search): GREEN - previne termo de busca vazio

--- web-frontend/modules/core/utils/search.js
+++ web-frontend/modules/core/utils/search.js
@@ -1,4 +1,6 @@
 export function highlightSearchTerm(text, term, options = {}) {
+  if (!term) return text
+  
   const tag = options.tag || 'mark'
   const flags = options.caseSensitive ? 'g' : 'gi'
   const regex = new RegExp(term, flags)
