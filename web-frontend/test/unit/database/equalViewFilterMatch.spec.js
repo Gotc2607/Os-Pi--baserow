3a616c667913710351ec08c9da07ce6be854aed1
JOAO VICTOR DA COSTA CRUZ <joao.cruz@ativossa.com.br>
2026-06-08 14:51:41 -0300

    test(equalFilter): RED C1 - valida igualdade e diferenca

---
+++ web-frontend/test/unit/database/equalViewFilterMatch.spec.js
@@ -0,0 +1,19 @@
+import { EqualViewFilterType } from '../../../modules/database/viewFilters.js'
+
+const equalFilter = new EqualViewFilterType({ app: { $registry: null } })
+const field = { type: 'text' }
+
+const equalMatches = (rowValue, filterValue) =>
+  equalFilter.matches(rowValue, filterValue, field, null)
+
+describe('EqualViewFilterType.matches - Testes de Caixa-Preta e Caixa-Branca', () => {
+  describe('C1: valores iguais e diferentes', () => {
+    test('C1-01: valor igual ao filtro retorna true', () => {
+      expect(equalMatches('Joao', 'Joao')).toBe(true)
+    })
+
+    test('C1-02: valor diferente do filtro retorna false', () => {
+      expect(equalMatches('Maria', 'Joao')).toBe(false)
+    })
+  })
+})
